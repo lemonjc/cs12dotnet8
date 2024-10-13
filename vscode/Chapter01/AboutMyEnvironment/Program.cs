186fe66e6eefb675cd519bab360c135496e7c8f2
Leon <indouf@qq.com>
2024-10-14 07:35:52 +0800

    Chapter01

---
+++ vscode/Chapter01/AboutMyEnvironment/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace AboutMyEnvironment;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(Environment.CurrentDirectory);
+        System.Console.WriteLine(Environment.OSVersion.VersionString);
+        System.Console.WriteLine("Namespace: {0}", typeof(Program).Namespace);
+    }
+}
